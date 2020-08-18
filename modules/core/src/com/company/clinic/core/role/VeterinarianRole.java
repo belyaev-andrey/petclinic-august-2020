0fcebb4774055b2385833e76c21effacc065e88d
Andrey Belyaev <belyaev.andrey@gmail.com>
2020-08-18 14:48:42 +0300

    day 2 part 2

---
+++ modules/core/src/com/company/clinic/core/role/VeterinarianRole.java
@@ -0,0 +1,43 @@
+package com.company.clinic.core.role;
+
+import com.company.clinic.entity.*;
+import com.haulmont.cuba.security.app.role.AnnotatedRoleDefinition;
+import com.haulmont.cuba.security.app.role.annotation.EntityAccess;
+import com.haulmont.cuba.security.app.role.annotation.EntityAttributeAccess;
+import com.haulmont.cuba.security.app.role.annotation.Role;
+import com.haulmont.cuba.security.app.role.annotation.ScreenAccess;
+import com.haulmont.cuba.security.entity.EntityOp;
+import com.haulmont.cuba.security.role.EntityAttributePermissionsContainer;
+import com.haulmont.cuba.security.role.EntityPermissionsContainer;
+import com.haulmont.cuba.security.role.ScreenPermissionsContainer;
+
+@Role(name = VeterinarianRole.NAME)
+public class VeterinarianRole extends AnnotatedRoleDefinition {
+    public final static String NAME = "veterinarian";
+
+    @ScreenAccess(screenIds = {"clinic_Visit.browse", "application-clinic", "clinic_UsedConsumables", "clinic_Consumable.browse", "clinic_Visit.edit"})
+    @Override
+    public ScreenPermissionsContainer screenPermissions() {
+        return super.screenPermissions();
+    }
+
+    @EntityAccess(entityClass = Visit.class, operations = EntityOp.UPDATE)
+    @EntityAccess(entityClass = Pet.class, operations = EntityOp.READ)
+    @EntityAccess(entityClass = PetType.class, operations = EntityOp.READ)
+    @EntityAccess(entityClass = Owner.class, operations = EntityOp.READ)
+    @EntityAccess(entityClass = Consumable.class, operations = EntityOp.READ)
+    @Override
+    public EntityPermissionsContainer entityPermissions() {
+        return super.entityPermissions();
+    }
+
+    @EntityAttributeAccess(entityClass = Visit.class, modify = "hoursSpent", view = {"pet", "veterinarian", "date", "description"})
+    @EntityAttributeAccess(entityClass = Pet.class, view = "*")
+    @EntityAttributeAccess(entityClass = PetType.class, view = "*")
+    @EntityAttributeAccess(entityClass = Owner.class, view = "*")
+    @EntityAttributeAccess(entityClass = Consumable.class, view = {"title", "price"})
+    @Override
+    public EntityAttributePermissionsContainer entityAttributePermissions() {
+        return super.entityAttributePermissions();
+    }
+}
